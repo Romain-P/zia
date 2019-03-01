f9fe5861ff9c355f9c7d1407d48cf17cfa889383
romain-p <romain.pillot@epitech.eu>
2019-03-01 07:40:44 +0100

    began parser class

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -2,4 +2,4 @@
 // Created by romain on 3/1/19.
 //
 
-#include "RequestParser.h"
+#include "Parser.h"
---
+++ src/Parser.h
@@ -0,0 +1,14 @@
+//
+// Created by romain on 3/1/19.
+//
+
+#ifndef ZIA_PARSER_H
+#define ZIA_PARSER_H
+
+
+class Parser {
+
+};
+
+
+#endif //ZIA_PARSER_H
