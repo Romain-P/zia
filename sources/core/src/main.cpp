b44443ee5ae515ede368153bc5e6bd58f1ff1b9f
romain-p <romain.pillot@epitech.eu>
2019-03-04 12:46:36 +0100

    added runtime exception handler for windows

--- sources/core/src/main.cpp
+++ sources/core/src/main.cpp
@@ -3,7 +3,28 @@
 #include "Console.h"
 #include <set>
 
+#ifdef _WIN32
+ #include <windows.h>
+ #include <sstream>
+
+LONG WINAPI TopLevelExceptionHandler(PEXCEPTION_POINTERS pExceptionInfo)
+{
+std::stringstream s;
+s << "Fatal: Unhandled exception 0x" << std::hex << pExceptionInfo->ExceptionRecord->ExceptionCode
+<< std::endl;
+
+MessageBoxA(NULL, s.str().c_str(), "my application", MB_OK | MB_ICONSTOP);
+exit(1);
+
+return EXCEPTION_CONTINUE_SEARCH;
+}
+#endif
+
+
 int main(int ac, char *args[]) {
+#ifdef _WIN32
+    SetUnhandledExceptionFilter(TopLevelExceptionHandler);
+#endif
     info("detected platform %s", server.config().platform_str().c_str());
     server.start();
 
