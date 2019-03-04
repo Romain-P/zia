1affe43fd2ebe6e14135f269c79bb66189ab2b6c
romain-p <romain.pillot@epitech.eu>
2019-03-04 12:50:47 +0100

    added runtime exception handler for windows

--- sources/core/src/main.cpp
+++ sources/core/src/main.cpp
@@ -6,17 +6,28 @@
 #ifdef _WIN32
  #include <windows.h>
  #include <sstream>
+ #incldue <string>
 
 LONG WINAPI TopLevelExceptionHandler(PEXCEPTION_POINTERS pExceptionInfo)
 {
-std::stringstream s;
-s << "Fatal: Unhandled exception 0x" << std::hex << pExceptionInfo->ExceptionRecord->ExceptionCode
-<< std::endl;
+    std::stringstream s;
+    s << "Fatal: Unhandled exception 0x" << std::hex << pExceptionInfo->ExceptionRecord->ExceptionCode
+    << std::endl;
 
-MessageBoxA(NULL, s.str().c_str(), "my application", MB_OK | MB_ICONSTOP);
-exit(1);
+    std::string message;
+    DWORD errorMessageID = ::GetLastError();
+    if(errorMessageID != 0) {
+        LPSTR messageBuffer = nullptr;
+        size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                                     NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
 
-return EXCEPTION_CONTINUE_SEARCH;
+        message = std::string(messageBuffer, size);
+        LocalFree(messageBuffer);
+    }
+
+    MessageBoxA(NULL, (s.str() + " : " + message).c_str(), "zia runtime error", MB_OK | MB_ICONSTOP);
+
+    return EXCEPTION_CONTINUE_SEARCH;
 }
 #endif
 
